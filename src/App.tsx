45c6eef3ea1744b93a02009c78a3faffb567e964
ogeobubu <ogeobubu@gmail.com>
2024-04-20 07:21:03 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./app.scss";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="Services">Parallex</section>
+      <section>Services</section>
+      <section id="Portfolio">Parallex</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+    </>
+  );
+}
+
+export default App;
