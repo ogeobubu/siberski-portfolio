7497abed66dd0a952dbba7ead66ed706e1c42ee5
Oge Obubu <ogeobubu@gmail.com>
2025-11-03 13:26:45 +0100

    Add Navbar component and breadcrumb navigation to Blog Page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "839dde5f56e34f49"; }
+/******/ 		__webpack_require__.h = function() { return "4d273d202bb5d67f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
